c8c309afc80e2654aa00df7fc886abfe7d5d55b1
lukaluks <163586723+lukaluks@users.noreply.github.com>
2025-02-16 15:53:28 +0200

    push

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
+
+
 const modal = document.getElementById("modal");
-document.getElementById("open-modal").onclick = () =>
-  (modal.style.display = "flex");
-document.getElementById("close-modal").onclick = () =>
-  (modal.style.display = "none");
+
+document.getElementById("open-modal").onclick = () => {
+  modal.style.display = "flex";
+};
+
+document.getElementById("close-modal").onclick = () => {
+  modal.style.display = "none";
+};
+
 modal.onclick = (e) => {
-  if (e === modal) modal.style.display = "none";
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
 };
 
 
-function changeBackgroundColor() {
-    document.body.style.backgroundColor = ;
-}
+const radios = document.querySelectorAll("input");
 
-const radioButtons = document.getElementsByName("color");
-for (const radio of radioButtons) {
-    radio.addEventListener("click", changeBackgroundColor);
-}
+radios.forEach(radio => {
+  radio.addEventListener("change", (event) => {
+    document.body.style.backgroundColor = event.target.value;
+  });
+
+});
+
+
+const nameInput = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
+
+nameInput.addEventListener("input", () => {
+  nameOutput.textContent = nameInput.value.trim() ? nameInput.value : "незнайомець"; //Тернарний оператор
+});
+
+
+const fontSizeControl = document.getElementById("font-size-control");
+const text = document.getElementById("text");
+
+fontSizeControl.addEventListener("input", () => {
+    text.style.fontSize = `${fontSizeControl.value}px`;
+});
